349e46b7a8276bde80746d5d494194464e3bb0b9
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-18 14:57:48 +0900

    add: デフォルトの言語・タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module WebAppDiary
       g.assets false
       g.helper false
     end
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
   end
 end
