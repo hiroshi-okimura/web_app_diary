60b34239ea711fb5382e58d0d43b48d6f0988b2a
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-21 11:17:50 +0900

    fix: rubocopによる修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   devise_for :users
-  resources :web_apps, only: [:index, :show] do
+  resources :web_apps, only: %i[index show] do
     collection do
       get :todayapp
     end
