9110cd0a74f8dab2b8bdde2adc8b2499b65aa32c
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-21 13:39:33 +0900

    add: omniauth_collbacks_controllerの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[line]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#top"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
+  
   resources :web_apps, only: %i[index show] do
     collection do
       get :todayapp
