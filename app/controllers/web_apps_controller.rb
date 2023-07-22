d368e3d37e6afa0846d3820d5039cdaf1060d413
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-22 22:19:15 +0900

    add: reviewの結果表示

--- app/controllers/web_apps_controller.rb
+++ app/controllers/web_apps_controller.rb
@@ -5,9 +5,15 @@ class WebAppsController < ApplicationController
 
   def show
     @web_app = WebApp.find(params[:id])
+    if user_signed_in?
+      @review = @web_app.reviews.find_by(user_id: current_user.id)
+    end
   end
 
   def todayapp
     @web_app = WebApp.first
+    if user_signed_in?
+      @review = @web_app.reviews.find_by(user_id: current_user.id)
+    end
   end
 end
