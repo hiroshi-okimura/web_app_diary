5438282a778b359b10fd578db9bb243663d0e034
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-22 16:46:46 +0900

    add: 記録フォームの作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,9 @@ class ReviewsController < ApplicationController
     @review = @web_app.reviews.build(review_params)
     @review.user_id = current_user.id
     if @review.save
-      redirect_to web_app_path(@web_app)
+      redirect_to web_app_path(@web_app), success: "レビューを記録しました"
     else
-      redirect_to web_app_path(@web_app)
+      redirect_to web_app_path(@web_app), danger: "レビューの記録に失敗しました"
     end
   end
 
