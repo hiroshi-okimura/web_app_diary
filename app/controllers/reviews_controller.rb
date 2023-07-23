b3db0dbb6a2c6e5232ae7edadff88e5a34316f3f
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-23 18:18:55 +0900

    fix: web_appとreview周りのリファクタリング

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,13 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update]
+  before_action :set_web_app, only: %i[new create edit update]
+  before_action :set_review, only: %i[edit update]
 
   def new
-    @web_app = WebApp.find(params[:web_app_id])
     @review = Review.new
   end
 
   def create
-    @web_app = WebApp.find(params[:web_app_id])
     @review = @web_app.reviews.build(review_params)
     @review.user_id = current_user.id
     if @review.save
@@ -17,18 +17,9 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def edit
-    @web_app = WebApp.find(params[:web_app_id])
-    if user_signed_in?
-      @review = @web_app.reviews.find_by(user_id: current_user.id)
-    end
-  end
+  def edit; end
 
   def update
-    @web_app = WebApp.find(params[:web_app_id])
-    if user_signed_in?
-      @review = @web_app.reviews.find_by(user_id: current_user.id)
-    end
     if @review.update(review_params)
       redirect_to web_app_path(@web_app), success: "レビューを更新しました"
     else
@@ -37,6 +28,14 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def set_web_app
+    @web_app = WebApp.find(params[:web_app_id])
+  end
+
+  def set_review
+    @review = @web_app.reviews.find_by(user_id: current_user.id)
+  end
 
   def review_params
     params.require(:review).permit(:originality_score, :usability_score, :design_score, :comment)
