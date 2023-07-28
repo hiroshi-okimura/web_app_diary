aae55501c366139e35e55f5fa1e187fa9aaaccb3
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-28 18:35:20 +0900

    add: 管理画面の作成

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,12 @@
+class Admin::BaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin
+#  layout 'admin/layouts/application'
+
+  private
+
+  def check_admin
+    redirect_to root_path,
+    notice: t('defaults.message.not_authorized') unless current_user.admin?
+  end
+end
