da4924b4e0e7ec9d916a16bfe7cd21c5682f32a3
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-28 11:09:04 +0900

    add: Userテーブルにroleカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_065256) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_020520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_065256) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
