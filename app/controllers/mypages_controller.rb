718a8b7e84b0d00dfbd18950b7b45d95791e3d5b
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-08-17 17:01:20 +0900

    add: カレンダーのビュー編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,5 +3,7 @@ class MypagesController < ApplicationController
 
   def show
     @bookmark_web_apps = current_user.bookmark_web_apps.order(created_at: :desc)
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @web_apps_with_reviews = WebApp.joins(:reviews).where(reviews: { user_id: current_user.id }).where(offer_date: start_date.beginning_of_month..start_date.end_of_month)
   end
 end
