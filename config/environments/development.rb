c0b1d1502cdf3c739f6bd0e8efe6f550840bdc76
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-08-25 12:24:34 +0900

    add: 開発環境でのパスワードリセット

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,15 @@ Rails.application.configure do
 
   # deviseの設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"] ,
+    authentication:       'plain',
+    enable_starttls_auto:  true
+    }
 end
