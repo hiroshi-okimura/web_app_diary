540c3fdb786cda0591583085b4dc077f4c00c1ca
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-09-01 14:47:13 +0900

    fix: dark_mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-27.times do |n|
+30.times do |n|
   WebApp.create(
-    site_name: "サイト#{n + 1}",
-    url: "https://www.site#{n + 1}.com",
-    ogp_title: "サイト#{n + 1}のOGPタイトル",
-    ogp_description: "サイト#{n + 1}のOGPディスクリプション",
+    site_name: "サイト#{n + 28}",
+    url: "https://www.site#{n + 28}.com",
+    ogp_title: "サイト#{n + 28}のOGPタイトル",
+    ogp_description: "サイト#{n + 28}のOGPディスクリプション",
     ogp_image: "ogp_image.png",
     screenshot: "not_found.png",
-    offer_date: Date.new(2023, 8, 5) + n.days,
+    offer_date: Date.new(2023, 9, 1) + n.days,
     status: 0
   )
 end
