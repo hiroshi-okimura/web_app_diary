f2b31fd37962fc8d42450ff17a09c46d72343c64
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-10-10 16:39:12 +0900

    ブックマークの非同期

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,14 +2,12 @@ class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    web_app = WebApp.find(params[:web_app_id])
-    current_user.bookmark(web_app)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @web_app = WebApp.find(params[:web_app_id])
+    current_user.bookmark(@web_app)
   end
 
   def destroy
-    web_app = current_user.bookmarks.find(params[:id]).web_app
-    current_user.unbookmark(web_app)
-    redirect_back fallback_location: root_path, status: :see_other, success: t('.success')
+    @web_app = current_user.bookmarks.find(params[:id]).web_app
+    current_user.unbookmark(@web_app)
   end
 end
