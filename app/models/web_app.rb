691b9f91adc6899d21d6af89c61c2aa57b6fb543
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-10-14 19:38:11 +0900

    add: uploder

--- app/models/web_app.rb
+++ app/models/web_app.rb
@@ -2,6 +2,7 @@ class WebApp < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   mount_uploader :screenshot, ScreenshotUploader
+  mount_uploader :this_ogp, OgpImageUploader
 
   validates :site_name, presence: true
   validates :url, presence: true, uniqueness: true
